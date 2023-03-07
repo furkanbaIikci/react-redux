8cc301b09187e3a3ed388c5afb7c9d5f0faaaf84
Furkan <furkan_blkc@hotmail.com>
2023-03-07 17:34:15 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Counter from './Components/Counter';
 import Notes from './Components/Notes';
 
 
 function App() {
   return (
     <div className="App">
-        {/* <Counter /> */}
         <Notes />
     </div>
   );
--- src/Stores/index.js
+++ src/Stores/index.js
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counter from "./counter";
 import notes from "./notes";
 export const store = configureStore({
     reducer: {
-        counter,
+        
         notes,
 
     },
